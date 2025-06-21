eea7506c6c395097e1d5e769d70744bdbe5384fe
valkyrie_pilot <valk@randomairborne.dev>
2025-06-21 13:13:37 -0600

    linux needs extensions

--- ci/signify.sh
+++ ci/signify.sh
@@ -12,7 +12,7 @@ mkdir -p dists/stable/main/binary-$arch;
 dpkg-scanpackages --arch $arch pool/ > dists/stable/main/binary-$arch/Packages
 cat dists/stable/main/binary-$arch/Packages | gzip -9 > dists/stable/main/binary-$arch/Packages.gz
 done
-./generate-release > dists/stable/Release
+./generate-release.sh > dists/stable/Release
 cat dists/stable/Release | gpg -abs > dists/stable/Release.gpg
 cat dists/stable/Release | gpg -abs --clearsign > dists/stable/InRelease
 gpg --export --armor > pgp-key.public
