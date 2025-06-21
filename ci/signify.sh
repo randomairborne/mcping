5808c9b4be8ac0ffb572393075f23b71c4c0b8a5
valkyrie_pilot <valk@randomairborne.dev>
2025-06-21 11:56:53 -0600

    Clean up signer script

--- ci/signify.sh
+++ ci/signify.sh
@@ -2,13 +2,16 @@
 set -e
 
 mkdir -p apt-repo/mcping/pool/main/
-mv *.deb apt-repo/mcping/pool/main/
+
+cd apt-repo/mcping/
+mv *.deb pool/main/
+
 for arch in "amd64" "arm64";
 do
 mkdir -p apt-repo/mcping/dists/stable/main/binary-$arch;
 dpkg-scanpackages --arch $arch pool/ > dists/stable/main/binary-$arch/Packages
 cat dists/stable/main/binary-$arch/Packages | gzip -9 > dists/stable/main/binary-$arch/Packages.gz
 done
-cat apt-repo/mcping/dists/stable/Release | gpg -abs > apt-repo/mcping/dists/stable/Release.gpg
-cat apt-repo/mcping/dists/stable/Release | gpg -abs --clearsign > apt-repo/mcping/dists/stable/InRelease
-gpg --export --armor > apt-repo/mcping/pgp-key.public
+cat dists/stable/Release | gpg -abs > dists/stable/Release.gpg
+cat dists/stable/Release | gpg -abs --clearsign > dists/stable/InRelease
+gpg --export --armor > pgp-key.public
