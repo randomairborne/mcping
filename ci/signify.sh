a878cd47b6d2b3ec216d72c7aeb5e410416f31dc
valkyrie_pilot <valk@randomairborne.dev>
2025-06-21 13:29:00 -0600

    remove mistakenly unedited cd command

--- ci/signify.sh
+++ ci/signify.sh
@@ -4,7 +4,7 @@ set -e
 mkdir -p apt-repo/mcping/pool/main/
 mv "$@" apt-repo/mcping/pool/main/
 
-cd apt-repo/mcping/dists/stable
+cd apt-repo/mcping/
 
 for arch in "amd64" "arm64";
 do
@@ -13,9 +13,9 @@ dpkg-scanpackages --arch $arch pool/ > dists/stable/main/binary-$arch/Packages
 cat dists/stable/main/binary-$arch/Packages | gzip -9 > dists/stable/main/binary-$arch/Packages.gz
 done
 
-cd dists/stable
+cd dists/stable/
 ../../../../ci/generate-release.sh > Release
-cd ../..
+cd ../../
 
 cat dists/stable/Release | gpg -abs > dists/stable/Release.gpg
 cat dists/stable/Release | gpg -abs --clearsign > dists/stable/InRelease
