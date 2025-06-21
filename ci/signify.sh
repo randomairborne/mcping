a342004687c4649057b747dc40ebc495a7b8a5b1
valkyrie_pilot <valk@randomairborne.dev>
2025-06-21 13:25:30 -0600

    properly generate releases

--- ci/signify.sh
+++ ci/signify.sh
@@ -4,7 +4,7 @@ set -e
 mkdir -p apt-repo/mcping/pool/main/
 mv "$@" apt-repo/mcping/pool/main/
 
-cd apt-repo/mcping/
+cd apt-repo/mcping/dists/stable
 
 for arch in "amd64" "arm64";
 do
@@ -13,7 +13,10 @@ dpkg-scanpackages --arch $arch pool/ > dists/stable/main/binary-$arch/Packages
 cat dists/stable/main/binary-$arch/Packages | gzip -9 > dists/stable/main/binary-$arch/Packages.gz
 done
 
-../../ci/generate-release.sh > dists/stable/Release
+cd dists/stable
+../../../../ci/generate-release.sh > Release
+cd ../..
+
 cat dists/stable/Release | gpg -abs > dists/stable/Release.gpg
 cat dists/stable/Release | gpg -abs --clearsign > dists/stable/InRelease
 gpg --export --armor > pgp-key.public
